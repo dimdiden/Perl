96fd0f4381d80f0087805ed6be9097a7e9350b74
dimdiden <dimdiden@gmail.com>
2014-12-20 11:29:21 +0200

    1 homework. It works when file "words" is in /home/

---
+++ homework/1.1hw.pl
@@ -0,0 +1,18 @@
+#!usr/bin/perl
+use strict;
+use warnings;
+
+open (FILE, "<","/home/words" ) or die "File not found\n";
+my @array = <FILE>;
+my @count = ();
+foreach my $word (@array) {
+	my $len = length ($word);
+if ($len >= 15){
+push(@count,$len);
+}
+}
+my $size = @count;
+print "Number of words in file: $size\n";
+
+
+
